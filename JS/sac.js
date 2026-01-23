a86265419b923ea4c235d8f148ba5987a9bcb5aa
Luis Mattos <lhenriqur007@gmail.com>
2026-01-23 00:01:02 -0300

    Update sac.js

--- JS/sac.js
+++ JS/sac.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'data-validade-input-expired', 'postagem-correios-template',
         'primeiro-ticket-options', 'ticket-expirado-options',
         'nf-input-postagem', 'crg-input', 'anexo-info-input',
-        'pendencias-options', 'edi-select', 'nf-select' // Adicionados para garantir o reset
+        'pendencias-options', 'edi-select', 'nf-select'
     ].reduce((acc, id) => {
         const el = document.getElementById(id);
         if (el) {
@@ -160,7 +160,7 @@ Cep: 43721-450 SIMOES FILHO/BA`
             'destinatario_container', 'tipo_operacao_container', 'pdaf_options',
             'solar_options', 'rma_fields', 'ticket_correios_options', 'email_preview', 
             'recusa_nf_options', 'primeiro_ticket_options', 'ticket_expirado_options',
-            'pendencias_options' // Adicionado para ocultar opções de Apoio ao mudar para SAC
+            'pendencias_options'
         ];
 
         containersToHide.forEach(id => {
@@ -174,9 +174,7 @@ Cep: 43721-450 SIMOES FILHO/BA`
             if (elements.postagem_correios_template) elements.postagem_correios_template.value = '';
         }
 
-        setVisibility(elements.ean_input, true);
-        setVisibility(elements.swqt_input, true);
-        setVisibility(camposExclusivosCorreios, true);
+        if (camposExclusivosCorreios) setVisibility(camposExclusivosCorreios, false);
     };
 
     // 3. Funções de Atualização de Email
@@ -272,18 +270,12 @@ Cep: 43721-450 SIMOES FILHO/BA`
         const produto = elements.produto_desc_input.value || '...';
         const dataRecebimento = elements.data_emissao_input.value || '__/__/____';
 
-        let obsSimoes = "";
-        if (destinatarioKey === 'simoes') {
-            obsSimoes = `<br><br><span style="color: #FF0000;"><b>ATENÇÃO:</b> Obrigatório agendamento prévio para Simões Filho via iemilli@toplogba.com.br / operacional@toplogba.com.br</span>`;
-        }
-
         if (destinatarioKey) {
             const emailText = TEMPLATES[templateKey]
                 .replace('{{saudacao}}', getSaudacao())
                 .replace('{{produto}}', produto)
                 .replace('{{data_recebimento}}', dataRecebimento)
-                .replace('{{destinatario}}', endereco)
-                .replace('{{observacao_simoes}}', obsSimoes);
+                .replace('{{destinatario}}', endereco);
             
             elements.email_content.innerHTML = emailText.trim();
             setVisibility(elements.email_preview, true);
@@ -350,14 +342,22 @@ Cep: 43721-450 SIMOES FILHO/BA`
                 setVisibility(elements.tipo_operacao_container, true); 
                 setVisibility(elements.rma_fields, true);
                 setVisibility(elements.solar_options, true);
+                // OCULTA campos desnecessários de RMA que vinham do Advanced
+                setVisibility(camposExclusivosCorreios, false);
             },
             solicitar_entrada_nf: () => setVisibility(elements.pdaf_options, true), 
             troca_solar: () => setVisibility(elements.solar_options, true), 
             envio_material_devolucao: () => { setVisibility(elements.destinatario_container, true); updateEnvioMaterialEmail(); },
             ticket_para_advanceds: () => { setVisibility(elements.ticket_correios_options, true); setVisibility(camposExclusivosCorreios, true); },
             recusa_nf: () => setVisibility(elements.recusa_nf_options, true),
-            advanced_emissao_envio: () => { setVisibility(elements.destinatario_container, true); setVisibility(elements.primeiro_ticket_options, true); setVisibility(camposExclusivosCorreios, false); },
-            advanced_apenas_envio: () => { setVisibility(elements.destinatario_container, true); setVisibility(elements.primeiro_ticket_options, true); setVisibility(camposExclusivosCorreios, false); }
+            advanced_emissao_envio: () => { 
+                setVisibility(elements.destinatario_container, true); 
+                setVisibility(camposExclusivosCorreios, true); // Exibe Descrição e Data de Recebimento
+            },
+            advanced_apenas_envio: () => { 
+                setVisibility(elements.destinatario_container, true); 
+                setVisibility(camposExclusivosCorreios, true); // Exibe Descrição e Data de Recebimento
+            }
         }
     };
 
